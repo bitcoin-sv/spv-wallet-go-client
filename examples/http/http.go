83e39560607f16e1a303a49de53f0b66783ffb6f
mrz1836 <mrz1818@gmail.com>
2022-03-08 14:42:28 -0500

    Added basic examples

---
+++ examples/http/http.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/BuxOrg/go-buxclient"
+	"github.com/bitcoinschema/go-bitcoin/v2"
+)
+
+func main() {
+
+	// Example xPub
+	masterKey, _ := bitcoin.GenerateHDKey(bitcoin.SecureSeedLength)
+	// rawXPub, _ := bitcoin.GetExtendedPublicKey(masterKey)
+
+	// Create a client
+	buxClient, err := buxclient.New(
+		buxclient.WithXPriv(masterKey.String()),
+		buxclient.WithHTTP("localhost:3001"),
+		buxclient.WithDebugging(true),
+		buxclient.WithSignRequest(true),
+	)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Printf("client loaded - bux debug: %v", buxClient.IsDebug())
+}
---
+++ examples/register_xpub/register_xpub.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/BuxOrg/bux"
+	"github.com/BuxOrg/go-buxclient"
+	"github.com/bitcoinschema/go-bitcoin/v2"
+)
+
+func main() {
+
+	// Example xPub
+	masterKey, _ := bitcoin.GenerateHDKey(bitcoin.SecureSeedLength)
+	rawXPub, _ := bitcoin.GetExtendedPublicKey(masterKey)
+
+	// Create a client
+	buxClient, err := buxclient.New(
+		buxclient.WithXPriv(masterKey.String()),
+		buxclient.WithHTTP("localhost:3001"),
+		buxclient.WithDebugging(true),
+		buxclient.WithSignRequest(true),
+	)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	if err = buxClient.RegisterXpub(
+		context.Background(), rawXPub, &bux.Metadata{"example_field": "example_data"},
+	); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println("registered xPub: " + rawXPub)
+}
