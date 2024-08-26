687ac6611f764c95ed54044f1a03cdeee44984a5
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-08-26 12:23:55 +0200

    fix(SPV-930): restore example admin key

--- examples/example_keys.go
+++ examples/example_keys.go
@@ -5,7 +5,7 @@ package examples
 
 const (
 	// ExampleAdminKey - example admin key
-	ExampleAdminKey string = ""
+	ExampleAdminKey string = "xprv9s21ZrQH143K3CbJXirfrtpLvhT3Vgusdo8coBritQ3rcS7Jy7sxWhatuxG5h2y1Cqj8FKmPp69536gmjYRpfga2MJdsGyBsnB12E19CESK"
 
 	// you can generate new keys using `task generate_keys`
 
