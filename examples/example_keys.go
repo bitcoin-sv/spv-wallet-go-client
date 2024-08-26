c191838d46d2e8b7122838c64d89b30d7b96b58f
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-08-26 12:23:15 +0200

    fix(SPV-930): no local-testing values

--- examples/example_keys.go
+++ examples/example_keys.go
@@ -5,15 +5,15 @@ package examples
 
 const (
 	// ExampleAdminKey - example admin key
-	ExampleAdminKey string = "xprv9s21ZrQH143K3CbJXirfrtpLvhT3Vgusdo8coBritQ3rcS7Jy7sxWhatuxG5h2y1Cqj8FKmPp69536gmjYRpfga2MJdsGyBsnB12E19CESK"
+	ExampleAdminKey string = ""
 
 	// you can generate new keys using `task generate_keys`
 
 	// ExampleXPriv - example private key
-	ExampleXPriv string = "xprv9s21ZrQH143K2BsvHcmggU1yLpSnw941TbxF2FEBpUMBA4Jv6vuiMn5AVSAJPQT4QNS6kwiYJSsB3QzQs7drzZdvCutAtG55cZzJGnmcBbp"
+	ExampleXPriv string = ""
 	// ExampleXPub - example public key
-	ExampleXPub string = "xpub661MyMwAqRbcEfxPPeJh3bxhtrHHLbmrppsqpddoNotA2re4eUDxuaPeLg279PVvSuuh6xpqyZxzjUvPpBbLeNEfW4jJTd2KLNQAGqufCe1"
+	ExampleXPub string = ""
 
 	// ExamplePaymail - example Paymail address
-	ExamplePaymail string = "test123@krzysiek.test.4chain.space"
+	ExamplePaymail string = ""
 )
