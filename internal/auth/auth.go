5c649a117b4a78359865c8b41a254f99ff1921be
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2024-10-28 13:08:37 +0100

    ci(SPV-1138): add separated linter files and fix warnings. (#4)

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,3 +1,5 @@
+// Package auth is responsible for handling the authentication of the requests to the server.
+// nolint: unused,nolintlint // FIXME: this file will be used soon
 package auth
 
 import (
@@ -15,7 +17,6 @@ import (
 	sighash "github.com/bitcoin-sv/go-sdk/transaction/sighash"
 	"github.com/bitcoin-sv/go-sdk/transaction/template/p2pkh"
 	"github.com/bitcoin-sv/spv-wallet-go-client/internal/cryptoutil"
-
 	"github.com/bitcoin-sv/spv-wallet/models"
 )
 
@@ -26,23 +27,29 @@ func GetSignedHex(dt *models.DraftTransaction, xPriv *bip32.ExtendedKey) (string
 	// we need to reset the inputs as we are going to add them via tx.AddInputFrom (ts-sdk method) and then sign
 	tx.Inputs = make([]*trx.TransactionInput, 0)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to parse hex, %w", err)
 	}
 
 	// Enrich inputs
 	for _, draftInput := range dt.Configuration.Inputs {
 		lockingScript, err := prepareLockingScript(&draftInput.Destination)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("failed to prepare locking script, %w", err)
 		}
 		unlockScript, err := prepareUnlockingScript(xPriv, &draftInput.Destination)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("failed to prepare unlocking script, %w", err)
 		}
-		tx.AddInputFrom(draftInput.TransactionID, draftInput.OutputIndex, lockingScript.String(), draftInput.Satoshis, unlockScript)
+		err = tx.AddInputFrom(draftInput.TransactionID, draftInput.OutputIndex, lockingScript.String(), draftInput.Satoshis, unlockScript)
+		if err != nil {
+			return "", fmt.Errorf("failed to add inputs to transaction, %w", err)
+		}
 	}
 
-	tx.Sign()
+	err = tx.Sign()
+	if err != nil {
+		return "", fmt.Errorf("failed to sign transaction, %w", err)
+	}
 	return tx.String(), nil
 }
 
@@ -80,23 +87,31 @@ func getDerivedKeyForDestination(xPriv *bip32.ExtendedKey, dst *models.Destinati
 	// Derive the child key (m/chain/num)
 	derivedKey, err := bip32.GetHDKeyByPath(xPriv, dst.Chain, dst.Num)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to derive key for unlocking input, %w", err)
 	}
 	// Handle paymail destination derivation if applicable
 	if dst.PaymailExternalDerivationNum != nil {
 		derivedKey, err = derivedKey.Child(*dst.PaymailExternalDerivationNum)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to derive key for unlocking paymail input, %w", err)
 		}
 	}
 	// Get the private key from the derived key
-	return bip32.GetPrivateKeyFromHDKey(derivedKey)
+	priv, err := bip32.GetPrivateKeyFromHDKey(derivedKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get private key for unlocking paymail input, %w", err)
+	}
+	return priv, nil
 }
 
 // Generate unlocking script using private key
 func getUnlockingScript(privateKey *ec.PrivateKey) (*p2pkh.P2PKH, error) {
 	sigHashFlags := sighash.AllForkID
-	return p2pkh.Unlock(privateKey, &sigHashFlags)
+	unlocked, err := p2pkh.Unlock(privateKey, &sigHashFlags)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create unlocking script, %w", err)
+	}
+	return unlocked, nil
 }
 
 // createSignature will create a signature for the given key & body contents
@@ -146,7 +161,7 @@ func createSignatureCommon(payload *models.AuthPayload, bodyString string, priva
 	// Signature, using bitcoin signMessage
 	sigBytes, err := bsm.SignMessage(privateKey, getSigningMessage(key, payload))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to sign message, %w", err)
 	}
 
 	payload.Signature = base64.StdEncoding.EncodeToString(sigBytes)
