f16c37df13896407aa2bd0a61c5e0343433a7acf
chris-4chain <152964795+chris-4chain@users.noreply.github.com>
2025-01-23 10:32:47 +0100

    feat(SPV-000): default AdminXPriv for example_keys & polishing (#316)

--- examples/example_keys.go
+++ examples/example_keys.go
@@ -1,14 +1,20 @@
 package examples
 
 const (
-	Alias   string = "test"
-	Domain  string = "example.com"
-	Paymail string = Alias + "@" + Domain
+	Alias   = "test"
+	Domain  = "example.com"
+	Paymail = Alias + "@" + Domain
 )
 
 const (
-	AdminXPriv string = ""
-	UserXPriv  string = ""
-	UserXPub   string = ""
-	AccessKey  string = ""
+	// AdminXPriv is used to authenticate as an admin in the spv-wallet.
+	// NOTE: The provided key is a default key that matches SPV Wallet's default configuration.
+	AdminXPriv string = "xprv9s21ZrQH143K3CbJXirfrtpLvhT3Vgusdo8coBritQ3rcS7Jy7sxWhatuxG5h2y1Cqj8FKmPp69536gmjYRpfga2MJdsGyBsnB12E19CESK"
+
+	// UserXPriv is used to authenticate as a user in the spv-wallet.
+	// You can generate a new key pair using "generate_keys" example.
+	UserXPriv string = ""
+
+	// UserXPub is a public part of the UserXPriv key. It is used to create a new user in the spv-wallet by the admin.
+	UserXPub string = ""
 )
